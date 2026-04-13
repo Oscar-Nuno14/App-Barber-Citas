85ead29cfd82131fcc8c6b8e73fcfbd96810edf0
Oscar Noel Nuño Verdín <oscarnoel1233@gmail.com>
2026-04-13 01:18:50 -0600

    Cambios

--- src/constants/barbers.js
+++
@@ -1,5 +0,0 @@
-export const barbers = [
-  { id: 1, name: "Barbero 1" },
-  { id: 2, name: "Barbero 2" },
-  { id: 3, name: "Barbero 3" }
-]
--- src/constants/services.js
+++
@@ -1,5 +0,0 @@
-export const services = [
-  { id: 1, name: "Corte", price: 120 },
-  { id: 2, name: "Barba", price: 180 },
-  { id: 3, name: "Corte + Barba", price: 200 }
-]
