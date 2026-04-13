53d5cfac3cf12369270da38f5c9211ccf2bdd4ed
Oscar Noel Nuño Verdín <oscarnoel1233@gmail.com>
2026-04-13 00:26:29 -0600

    Constantes: barberos, servicios y horarios

---
+++ src/constants/barbers.js
@@ -0,0 +1,5 @@
+export const barbers = [
+  { id: 1, name: "Barbero 1" },
+  { id: 2, name: "Barbero 2" },
+  { id: 3, name: "Barbero 3" }
+]
---
+++ src/constants/services.js
@@ -0,0 +1,5 @@
+export const services = [
+  { id: 1, name: "Corte", price: 120 },
+  { id: 2, name: "Barba", price: 180 },
+  { id: 3, name: "Corte + Barba", price: 200 }
+]
